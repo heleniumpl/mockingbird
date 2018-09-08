24a7ee8f204ffefac2afcf0992afce93d02473f5
Michał Mally <michal.mally@getbase.com>
2018-09-08 03:58:48 +0200

    Spek for running on specified port fixed.

--- src/test/kotlin/MockingbirdSpec.kt
+++ src/test/kotlin/MockingbirdSpec.kt
@@ -4,7 +4,6 @@ import io.kotlintest.matchers.collections.shouldBeUnique
 import io.kotlintest.matchers.numerics.shouldBeGreaterThan
 import io.kotlintest.shouldBe
 import org.spekframework.spek2.Spek
-import org.spekframework.spek2.dsl.Skip.Yes
 import org.spekframework.spek2.style.specification.describe
 import java.net.ServerSocket
 
@@ -14,29 +13,29 @@ class MockingbirdSpec : Spek({
 
         context("when mock is started with no given port") {
             val mock by memoized { Mockingbird() }
-            beforeEach(mock::start)
+            beforeEach { mock.start() }
 
             it("starts on random free port > 1024") {
                 mock.port().shouldBeGreaterThan(1024)
             }
 
-            afterEach(mock::stop)
+            afterEach { mock.stop() }
         }
 
-        context("when mock is started with given (free) port", skip = Yes("Need to investigate why specified port does not work as expected")) {
-            val freePort by memoized { ServerSocket(0).use { it.reuseAddress = true; it.localPort } }
+        context("when mock is started with given (free) port") {
+            val freePort by memoized { freeTcpPort() }
             val mock by memoized { Mockingbird(freePort) }
-            beforeEach(mock::start)
+            beforeEach { mock.start() }
 
             it("starts on given port") {
                 mock.port() shouldBe freePort
             }
 
-            afterEach(mock::stop)
+            afterEach { mock.stop() }
         }
 
         context("when multiple mocks are run") {
-            val mocks by memoized { List(2) { Mockingbird() } }
+            val mocks by memoized { List(5) { Mockingbird() } }
 
             beforeEach { mocks.forEach(Mockingbird::start) }
 
@@ -50,3 +49,8 @@ class MockingbirdSpec : Spek({
     }
 
 })
+
+private fun freeTcpPort() = ServerSocket(0).use {
+    it.reuseAddress = true
+    it.localPort
+}
