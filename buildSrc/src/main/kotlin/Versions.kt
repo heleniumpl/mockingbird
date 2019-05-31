74ca201170420bb2c123899e1b0421b350ce17e6
Michał Mally <michal@mally.pl>
2019-06-01 00:15:30 +0200

    Dependencies updated.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,23 +1,23 @@
 object Versions {
 
-    const val kotlin = "1.3.21"
+    const val kotlin = "1.3.31"
 
     const val logback = "1.2.3"
     const val kotlinLogging = "1.6.26"
-    const val spark = "2.9.0"
-    const val jackson = "2.9.8"
+    const val spark = "2.9.1"
+    const val jackson = "2.9.9"
 
     // test
 
-    const val spek2 = "2.0.1"
+    const val spek2 = "2.0.5"
     const val kotlinTest = "3.3.2"
     const val mockk = "1.9.3"
 
-    const val fuel = "2.0.1"
+    const val fuel = "2.1.0"
 
     // gradle
 
-    const val gradle = "5.3.1"
+    const val gradle = "5.4.1"
 
     object GradlePlugins {
 
